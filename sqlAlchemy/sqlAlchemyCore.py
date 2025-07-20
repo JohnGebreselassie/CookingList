3e2263378a5d080cb1c1219a3199b197e425472c
John Gebreselassie <real.john.gebre@gmail.com>
2025-07-20 01:33:28 -0700

    sqlAlchemy ORM/Core reference code

--- sqlAlchemy/sqlAlchemyCore.py
+++ sqlAlchemy/sqlAlchemyCore.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Float, ForeignKey, func
 
 #engine = create_engine('sqlite:///mydatabase.db', echo = True) #sqllite version
 engine = create_engine('postgresql+psycopg2://postgres:4641@localhost:5432/satutorialdatabase', echo = True) #postgres version
@@ -10,7 +10,89 @@ people = Table( #creates table in database called people
     "people",
     meta,
     Column('id', Integer, primary_key=True),
-    Column('name', String, nullable=False)
+    Column('name', String, nullable=False), #makes it a required field
+    Column('age', Integer) 
 )
 
+things = Table( #creates table in database called people
+    "things",
+    meta,
+    Column('id', Integer, primary_key=True),
+    Column('description', String, nullable=False), 
+    Column('price', Float),
+    Column('owner', Integer, ForeignKey("people.id")) #creates a one to many relationship - each item is mapped to a person
+)
 meta.create_all(engine)
+
+conn = engine.connect()
+
+#inserting data - commented out because you dont always want to insert when running this code
+'''
+insert_statement = people.insert().values(name ='Mike',age=30)
+result = conn.execute(insert_statement)
+conn.commit() 
+
+insert_statement = people.insert().values(name ='Jane',age=32)
+result = conn.execute(insert_statement)
+conn.commit()
+
+insert_people = people.insert().values([
+    {'name' : "Josh", 'age':35},
+    {'name' : "Mary", 'age':70},
+    {'name' : "Benedict", 'age':65},
+    {'name' : "Thomas", 'age':22},
+    {'name' : "Jones", 'age':18}
+    ])
+insert_things = things.insert().values([
+    {'description' : "table", 'price':35.0, 'owner':3}, #bad practice to pick owner ids manually
+    {'description' : "tv", 'price':100.0, 'owner':4},
+    {'description' : "laptop", 'price':1500.50, 'owner':4},
+    {'description' : "pen", 'price':1.0, 'owner':5},
+    {'description' : "water bottle", 'price':10.0, 'owner':6},
+    {'description' : "tissues", 'price':5.0, 'owner':7}
+    ])
+
+conn.execute(insert_people)
+conn.commit() #have to commit before executing things because those 'people' ids need to exist in the database before mapping 'things' to them
+conn.execute(insert_things)
+conn.commit()
+'''
+
+#selecting data
+select_statement = people.select().where(people.c.age> 30)
+result = conn.execute(select_statement)
+
+#for row in result.fetchall():
+#    print(row)
+
+#updating data - commenting out because of commit, same as inserting
+'''
+update_statement = people.update().where(people.c.id == 2).values(name = 'James', age = 52)
+result = conn.execute(update_statement)
+conn.commit()
+'''
+
+#delete statement
+'''
+delete_statement = people.delete().where(people.c.id == 2)
+result = conn.execute(delete_statement)
+conn.commit()
+'''
+
+#join statement
+join_statement = people.join(things, people.c.id == things.c.owner) #inner join - people who dont own something aren't included
+#join_statement = people.outerjoin(things, people.c.id == things.c.owner) #outer join - everyone included, even if they don't own something
+select_statement = people.select().with_only_columns(people.c.name, things.c.description).select_from(join_statement) #include name, description
+result = conn.execute(select_statement)
+for row in result.fetchall():
+    print(row)
+
+
+#group statement
+group_by_statement = things.select().with_only_columns(things.c.owner, func.sum(things.c.price)).group_by(things.c.owner).having(func.sum(things.c.price) > 25) 
+    #when you group, you have to decide what you are doing with the values
+    #for example, if people own multiple items, and they only get one row, what are you gonna do with all their sums
+        #this example code sums up all their sums, only displaying if that sum is > 35
+result = conn.execute(group_by_statement)
+for row in result.fetchall():
+    print(row)
---
+++ sqlAlchemy/sqlAlchemyORM.py
@@ -0,0 +1,77 @@
+from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey, func
+import pandas as pd
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship #declarative_base replaces metadata from Core, and sessionmaker cause ORM uses sessions, not connections
+
+#orm maps python classes to database
+engine = create_engine('postgresql+psycopg2://postgres:4641@localhost:5432/satutorialdatabase', echo = True) #postgres version
+    #can read more about this in sqlAlchemyCore
+
+Base = declarative_base()
+
+class Person(Base):
+    __tablename__ = 'people' #name of database
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    age = Column(Integer)
+
+    things = relationship('Thing',back_populates='person') #defines a relationship between person/thing
+
+class Thing(Base):
+    __tablename__ = 'things' #name of database
+    id = Column(Integer, primary_key=True)
+    description = Column(String, nullable=False)
+    price = Column(Float)
+    owner = Column(Integer, ForeignKey("people.id")) 
+
+    person = relationship('Person', back_populates="things")
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+#creating a new person and thing mapped to a person
+'''
+newPerson = Person(name = 'Charlie', age = '70')
+session.add(newPerson)
+session.flush()
+
+newThing = Thing(description = 'lamp', price = '500', owner = newPerson.id) #much better practice to not manually put ids
+session.add(newThing)
+session.commit()
+
+#super convenient way to get things from the database using python, advantage of ORM 
+print([i.description for i in newPerson.things]) #put it in a list - some generator object bs that i forgot from cse20 lol!
+print(newThing.person.name)
+'''
+#select
+result = session.query(Person.name, Person.age).filter(Person.age>50).all() #.all() = all results that you get
+#if you were to put .query(Person), you would get a list of Person objects with attributes
+#print(result)
+
+#delete
+'''
+result = session.query(Person.name, Person.age).filter(Person.age>50).delete()
+session.commit()
+'''
+
+#update 
+'''
+result = session.query(Person).filter(Person.name == 'Charlie').update({'name':'Charles'}) #you have to put person cause you're updating the object
+session.commit()
+result = session.query(Person.name, Person.age).filter(Person.name == 'Charles').all() 
+print(result)
+'''
+
+#join
+result = session.query(Person.name, Thing.description).join(Person).all() #fun fact: in this query person and thing are interchangeable
+#print(result)
+
+result = session.query(Thing.owner, func.sum(Thing.price)).group_by(Thing.owner).having(func.sum(Thing.price) > 50).all() #fun fact: in this query person and thing are interchangeable
+#print(result)
+
+#pandas incorporation
+df = pd.read_sql("Select * from people", con = engine)
+print(df)
+#can also inser pandas dataframes into the database, but i didn't write that here - look at the vid if want
+session.close()
